9fbe48b59efd5165f2480c15df865ea3ca92a2d3
Ashmit-Parandkar <113757864+Ashmit-Parandkar@users.noreply.github.com>
2024-02-09 12:57:57 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const http = require("http");
-const WebSocket = require("ws");
 require("dotenv").config();
+const {createServer} = require('http');
+const { Server } = require('socket.io');
 
 const app = express();
 
@@ -15,41 +15,43 @@ app.use(express.json());
 
 let currentCode = "";
 
-const server = http.createServer(app); // Create an HTTP server
+// Initialize server with express app
+// const server = require('http').createServer(app);
+const server = new createServer(app);
+// const io = require('socket.io')(server);
+const io = new Server(server,{
+  cors:{
+     origin:"http://localhost:5173", 
+     method: ["GET","POST"],
+     credentials:true,
+  },
+ });
 
-// Initialize WebSocket server with the HTTP server instance
-const wss = new WebSocket.Server({ port: 9090 });
+// Socket.io server handling
+io.on('connection', (socket) => {
+  console.log('Socket.io client connected');
 
-// WebSocket server handling
-wss.on('connection', (ws) => {
-  console.log('WebSocket client connected');
-  ws.send(currentCode);
+  // Send current code to the newly connected client
+  socket.emit('currentCode', currentCode);
 
-  
-ws.on("message", (message) => {
-  console.log(`Received message: ${message}, ${message.data}`);
-  if (message.data === "getInitialCode") {
-    // If the message is a request for initial code, send the current code
-    ws.send(currentCode);
-  } else {
-    // Update the current code with the received message
-    currentCode = message;
-    // Broadcast the updated code to all connected clients
-    wss.clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(currentCode);
-        console.log(currentCode)
-      }
-    });
-  }
-});
+  socket.on('message', (message) => {
+    console.log(`Received message: ${message}, ${message.data}`);
+    if (message.data === "getInitialCode") {
+      // If the message is a request for initial code, send the current code
+      socket.emit('currentCode', currentCode);
+    } else {
+      // Update the current code with the received message
+      currentCode = message;
+      // Broadcast the updated code to all connected clients
+      io.emit('currentCode', currentCode);
+      console.log(currentCode);
+    }
+  });
 
-  
-  ws.on('close', () => {
-    console.log('WebSocket client disconnected');
+  socket.on('disconnect', () => {
+    console.log('Socket.io client disconnected');
   });
 });
-
 
 // Config Database
 mongoose
@@ -71,6 +73,6 @@ const messageRoutes = require("./routes/messages");
 app.use("/api", roomRoutes);
 app.use("/api", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
